50e838e4cd7c72c1171a8d397f0671ba5704c7fa
Michael Urquhart <michaelurquhart@hotmail.com>
2019-05-17 15:45:42 +0100

    pet_shop.rb passes test_increase_pets_sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -13,3 +13,9 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
 end
+
+def increase_pets_sold(pet_shop, additional_pets_sold)
+  pet_shop[:admin][:pets_sold] += additional_pets_sold
+  # pets_sold(pet_shop) += additional_pets_sold
+  # I thought the above commented-out line would work but it doesn't
+end
